dbb8b075f9146efde41de1117cc820545610bf04
Exsoflex <hectorlucioholguin@gmail.com>
2026-02-23 10:27:54 -0600

    Create '/ciudades' route to query cities
    Added a new route to fetch cities from the database.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/ciudades')
+def consultas():
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="",
+      database="clima_app"
+    )
+    
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM ciudades")
+    myresult = mycursor.fetchall()
+    return myresult
