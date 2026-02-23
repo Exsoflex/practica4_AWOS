6cf8b65e6fd7eefc68ffe6f1a26d5e26c5ccf6bb
Exsoflex <hectorlucioholguin@gmail.com>
2026-02-23 10:40:41 -0600

    Remove unused import 'report' from app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, report, justify, make_response, session
+from flask import Flask, render_template, justify, make_response, session
 app = Flask(__name__)
 
 @app.route('/ciudades')
