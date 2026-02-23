b6dc27a57dfb350b220acf01fc9f90abcfa9f083
Exsoflex <hectorlucioholguin@gmail.com>
2026-02-23 10:34:09 -0600

    Enhance consultas to format city results
    Updated the consultas function to format the result using justify and make_result.

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, report, justify, make_response, session
 app = Flask(__name__)
 
 @app.route('/ciudades')
 def consultas():
     import mysql.connector
-
     mydb = mysql.connector.connect(
       host="localhost",
       user="root",
       password="",
       database="clima_app"
     )
-    
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM ciudades")
     myresult = mycursor.fetchall()
-    return myresult
+    return make_result(justify(myresult))
