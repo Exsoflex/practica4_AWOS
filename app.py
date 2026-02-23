c95ac5bf233b1b0fdc2a1e417a9663efb3368cc2
Exsoflex <hectorlucioholguin@gmail.com>
2026-02-23 10:46:52 -0600

    Fix typo in import statement for 'jsonify'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, justify, make_response, session
+from flask import Flask, render_template, request, jonify, make_response, session
 app = Flask(__name__)
 
 @app.route('/ciudades')
